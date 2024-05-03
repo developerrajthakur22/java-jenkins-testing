56ea3f2feb9deea79086e89d4ed9e4bfbae0097a
Raj Thakur <rajt4900@gmail.com>
2024-05-03 14:04:21 +0530

    Bubble sort code

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 5};
         bubbleSort(arr);
+        System.out.println("Adding this print statement to check the deployment automation in Jenkins");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
